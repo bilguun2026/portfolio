40bfcd7a69c12e92af2083b6f2a8d4a26cf5e8a5
Bilguun <enkhbil2026@gmail.com>
2024-03-29 00:11:05 +0800

    update

--- protfolio/src/pages/Flashcard.tsx
+++ protfolio/src/pages/Flashcard.tsx
@@ -24,46 +24,48 @@ const FlashcardMakerPage: FC = () => {
       <Link to="/">
         <div className="text-bold text-[50px]">EDU+</div>
       </Link>
-      <h1 className="text-3xl font-bold my-4">Flashcard Maker</h1>
-      <div className="mb-4 bg-white flex flex-row border rounded-xl p-2">
-        <input
-          type="text"
-          placeholder="Enter question"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-          className="border p-2 mr-2 w-2/5"
-        />
-        <div className="bg-zinc-400 w-0.5 h-auto"></div>
-        <input
-          type="text"
-          placeholder="Enter answer"
-          value={answer}
-          onChange={(e) => setAnswer(e.target.value)}
-          className="border ml-2 p-2 w-3/5"
-        />
-        <button
-          onClick={handleAddFlashcard}
-          className="bg-green-500 w-12 h-12 flex items-center justify-center text-white font-semibold py-2 px-4 rounded-md transition duration-300 hover:bg-blue-600"
-        >
-          <FaPlus size={24} />
-        </button>
-      </div>
-      {flashcards.length > 0 && (
-        <div className="">
-          {flashcards.map((flashcard, index) => (
-            <Flashcard
-              key={index}
-              question={flashcard.question}
-              answer={flashcard.answer}
-              onDelete={() => {
-                if (typeof index === "number") {
-                  deleteFlashcard(index);
-                }
-              }}
-            />
-          ))}
+      <div className="px-4">
+        <h1 className="text-3xl font-bold my-4">Flashcard Maker</h1>
+        <div className="mb-4 bg-white flex flex-row border rounded-xl p-2">
+          <input
+            type="text"
+            placeholder="Enter question"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            className="border p-2 mr-2 w-2/5"
+          />
+          <div className="bg-zinc-400 w-0.5 h-auto"></div>
+          <input
+            type="text"
+            placeholder="Enter answer"
+            value={answer}
+            onChange={(e) => setAnswer(e.target.value)}
+            className="border ml-2 p-2 w-3/5"
+          />
+          <button
+            onClick={handleAddFlashcard}
+            className="bg-green-500 w-12 h-12 flex items-center justify-center text-white font-semibold py-2 px-4 rounded-md transition duration-300 hover:bg-blue-600"
+          >
+            <FaPlus size={24} />
+          </button>
         </div>
-      )}
+        {flashcards.length > 0 && (
+          <div className="">
+            {flashcards.map((flashcard, index) => (
+              <Flashcard
+                key={index}
+                question={flashcard.question}
+                answer={flashcard.answer}
+                onDelete={() => {
+                  if (typeof index === "number") {
+                    deleteFlashcard(index);
+                  }
+                }}
+              />
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
